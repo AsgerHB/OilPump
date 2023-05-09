cbfa2341e1a9813fd71dbbf8c332032dbac740ec
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-09 13:21:27 +0200

    Revert

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# export NAME=Default
+export NAME=Default
 
-# for RUNS in 100 1500 3000 6000 12000 24000 48000 96000 192000 384000 ; do
-#     for LR in 2 4 ; do 
-#         export LR=$LR
-#         export RUNS=$RUNS
-#         $EXECUTOR ./run_single.sh
-#     done
-# done
+for RUNS in 100 1500 3000 6000 12000 24000 48000 96000 192000 384000 ; do
+    for LR in 2 4 ; do 
+        export LR=$LR
+        export RUNS=$RUNS
+        $EXECUTOR ./run_single.sh
+    done
+done
 
 export NAME=Div100
 
