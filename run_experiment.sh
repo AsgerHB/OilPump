46b25bb2f5518c9dc12de02162024f0d5d44a46f
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-10 10:53:01 +0200

    See if more memory does it

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,7 +2,7 @@
 
 export NAME=Default
 
-for RUNS in 100 1500 3000 6000 12000 24000 48000 96000 192000 384000 ; do
+for RUNS in 48000 96000 192000 384000 ; do
     for LR in 2 4 ; do 
         export LR=$LR
         export RUNS=$RUNS
@@ -12,7 +12,7 @@ done
 
 export NAME=Div100
 
-for RUNS in 100 1500 3000 6000 12000 24000 48000 96000 192000 384000 ; do
+for RUNS in 48000 96000 192000 384000 ; do
     for LR in 2 4 ; do 
         export LR=$LR
         export RUNS=$RUNS
--- run_slurm.sh
+++ run_slurm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export EXECUTOR="sbatch --out=/dev/null --partition=rome -n1 --mem=1G --time=3:30:00 "
+export EXECUTOR="sbatch --out=/dev/null --partition=rome -n1 --mem=3G --time=3:30:00 "
 ./run_experiment.sh
