26ed888af9a3d9ad5b9aacfa72e8bd6324e211c4
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-24 15:08:03 +0200

    Update to new binary

--- run_single.sh
+++ run_single.sh
@@ -6,5 +6,5 @@ mkdir -p $DIR
 cd $DIR
 echo "$DIR"
 
-R=$( /usr/bin/time -v $CUR/uppaal-4.1.20-stratego-10-linux64/bin/verifyta.sh -s --good-runs $RUNS --total-runs $RUNS --max-iterations 1 --eval-runs 1 $CUR/$NAME/OP__PreShielded.xml 2>&1)
+R=$( /usr/bin/time -v $CUR/uppaal-533581a/verifyta.sh -s --good-runs $RUNS --total-runs $RUNS --max-iterations 1 --eval-runs 1 $CUR/$NAME/OP__PreShielded.xml 2>&1)
 echo "$R" > out
